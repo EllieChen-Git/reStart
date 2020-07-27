1d17af551bf9763155bd88095818587d12d1b7e7
Ellie <elliechen.etc@gmail.com>
2020-07-27 12:06:54 +1000

    Lab 1- Added comments & README

--- Insulin.py
+++ Insulin.py
@@ -1,9 +1,16 @@
 clean_text = "malwmrllpl lallalwgpd paaafvnqhl cgshlvealy lvcgergffy tpktrreaed lqvgqvelgg gpgagslqpl alegslqkrg iveqcctsic slyqlenycn"
+
 removed_space = clean_text.replace(" ", "")
+# Use .replace() method to remove spaces in 'clean_text': https://www.w3schools.com/python/ref_string_replace.asp
 print("Total Word count: " + str(len(removed_space)))
+# Use .len() function to return the length of 'removed_space': https://www.w3schools.com/python/ref_func_len.asp
+# Use .str() function to convert numbers to strings: https://www.w3schools.com/python/ref_func_str.asp
 print("=====")
+# Just a seperater to make the output looks nicer
 
 lsinsulin_seq_clean = removed_space[0:24]
+# Split string into sub-strings [beginningIndex:endingIndex] (where 'beginningIndex' is included but 'endingIndex' is not included): https://www.w3schools.com/python/gloss_python_string_slice.asp
+# Note: Programming languages count from 0. 
 print("Word count - lsinsulin_seq_clean: " + str(len(lsinsulin_seq_clean)))
 print("lsinsulin: " + lsinsulin_seq_clean)
 print("=====")
---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS re/Start Program - Code Snippet
+
+- ##### Python Lab 1 - [Insulin](https://github.com/EllieChen-Git/reStart/blob/master/Insulin.py)
